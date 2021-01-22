a28a84d91b898506b1912dfad91289bf18b5f10d
tmcarron <tmcarron7@gmail.com>
2021-01-22 10:47:54 -0500

    Made a program that simplified regex using regex

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Simplify_Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter a fraction");
+                string fraction = Console.ReadLine();
+                if (Regex.IsMatch(fraction, @"^[0-9]+\/[0-9]+$"))
+                {
+                    Console.WriteLine(Simplify(fraction));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that is not a correctly formatted fraction. Please try again.");
+                }
+            }
+            
+        }
+        static string Simplify(string fraction)
+        {
+            string[] stringNums = fraction.Split('/');
+            int num1 = Convert.ToInt32(stringNums[0]);
+            int num2 = Convert.ToInt32(stringNums[1]);
+            
+            for(int i = num1; i > 0; i--)
+            {
+                if(num1 % i == 0 && num2 % i == 0)
+                {
+                    num1 /= i;
+                    num2 /= i;
+                }
+            }
+            return num1 + "/" + num2;
+        }
+    }
+}
